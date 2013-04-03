bf822458a3d518c175e4e666a37d1459f70f6fc1
zyberspace <zyberspace@zyberware.org>
2013-04-03 20:48:09 +0200

    Better SQL-Dump
    - no comments
    - official Teamspeak Public Server as testserver
    - fix: Table basic has noch lastLookup-Column, so changed the additionalColumns-Column

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -8,11 +9,22 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `basic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `basic` (
+  `domain` varchar(255) NOT NULL,
+  `address` varchar(60) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`domain`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `serverDefault`
---
-
+LOCK TABLES `basic` WRITE;
+/*!40000 ALTER TABLE `basic` DISABLE KEYS */;
+INSERT INTO `basic` VALUES ('localhost','voice.teamspeak-systems.de:9987',1);
+/*!40000 ALTER TABLE `basic` ENABLE KEYS */;
+UNLOCK TABLES;
 DROP TABLE IF EXISTS `serverDefault`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -22,20 +34,11 @@ CREATE TABLE `serverDefault` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Contains a default server, if the requested domain was not found (set active=1 to activate)';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `serverDefault`
---
-
 LOCK TABLES `serverDefault` WRITE;
 /*!40000 ALTER TABLE `serverDefault` DISABLE KEYS */;
 INSERT INTO `serverDefault` VALUES ('127.0.0.1:9987',0);
 /*!40000 ALTER TABLE `serverDefault` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `serverTables`
---
-
 DROP TABLE IF EXISTS `serverTables`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -48,39 +51,10 @@ CREATE TABLE `serverTables` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Contains all serverTables, that should be used for the lookup';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `serverTables`
---
-
 LOCK TABLES `serverTables` WRITE;
 /*!40000 ALTER TABLE `serverTables` DISABLE KEYS */;
-INSERT INTO `serverTables` VALUES ('basic','lastLookup',1,1);
+INSERT INTO `serverTables` VALUES ('basic','',1,1);
 /*!40000 ALTER TABLE `serverTables` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `basic`
---
-
-DROP TABLE IF EXISTS `basic`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `basic` (
-  `domain` varchar(255) NOT NULL,
-  `address` varchar(60) NOT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`domain`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `basic`
---
-
-LOCK TABLES `basic` WRITE;
-/*!40000 ALTER TABLE `basic` DISABLE KEYS */;
-INSERT INTO `basic` VALUES ('localhost','localhost:9987',1);
-/*!40000 ALTER TABLE `basic` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -91,3 +65,4 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
